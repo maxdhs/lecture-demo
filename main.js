ff8bccaa0af0cad5026df62ba01c090404abe9b7
Maxwell Dubin <maxdhs@gmail.com>
2022-07-14 19:10:47 -0400

    commit 1

---
+++ main.js
@@ -0,0 +1,10 @@
+// git
+// "version control" - just a way to save our code
+// local
+
+
+// github
+// save your files on the web, remote backup in the cloud and collaboration
+
+// first step to begin to use git
+// 
