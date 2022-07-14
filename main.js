b090248ac2b27ddda15e5f9db725436704779697
Maxwell Dubin <maxdhs@gmail.com>
2022-07-14 19:13:02 -0400

    commit 2

--- main.js
+++ main.js
@@ -2,9 +2,16 @@
 // "version control" - just a way to save our code
 // local
 
-
 // github
 // save your files on the web, remote backup in the cloud and collaboration
 
 // first step to begin to use git
-// 
+// make sure you're in the right folder
+// git init
+
+// how to save?
+// git add . (to stage all our files get ready to commit)
+// git commit -m "any message"
+
+let x = 2;
+console.log(x);
